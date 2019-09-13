7bc24688cc6aada57a4c4f874d76335af8f7cab3
Сидоров Константин <sidorovalpha@gmail.com>
2019-09-13 21:11:29 +0300

    routing in separated component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "react-router";
 import { createBrowserHistory } from "history";
-import { Router, Switch, Route } from "react-router";
-import { Home } from "./pages/home";
-import * as routes from "./constants/routes";
 import { createGlobalStyle } from "styled-components";
-import { Start } from "./pages/start";
-import { Game } from "./pages/game";
-import { Finish } from "./pages/finish";
+import { Routing } from './routing'
 
 export const history = createBrowserHistory();
 
@@ -25,12 +21,7 @@ const App = () => (
   <>
     <GlobalStyle />
     <Router history={history}>
-      <Switch>
-        <Route exact path={routes.home} component={Home} />
-        <Route path={routes.start} component={Start} />
-        <Route path={routes.game} component={Game} />
-        <Route path={routes.finish} component={Finish} />
-      </Switch>
+        <Routing />
     </Router>
   </>
 );
---
+++ src/routing.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Switch, Route } from "react-router";
+import * as routes from "./constants/routes";
+import { Home } from "./pages/home";
+import { Start } from "./pages/start";
+import { Game } from "./pages/game";
+import { Finish } from "./pages/finish";
+
+export const Routing = () =>
+  <Switch>
+    <Route exact path={routes.home} component={Home} />
+    <Route path={routes.start} component={Start} />
+    <Route path={routes.game} component={Game} />
+    <Route path={routes.finish} component={Finish} />
+  </Switch>
